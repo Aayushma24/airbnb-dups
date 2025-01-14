31375442f6d9f038ddeb782d983fb57aed928f29
Aayushma Khanal <aayushmakhanal10@gmail.com>
2025-01-14 18:40:23 +0545

    make footer and navbar

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './footer.module.css'
+export default function Footer() {
+
+  return (
+    <footer>
+        <div className={styles.container}> 
+            <ul>
+                <b> Support</b>
+                <li>Help Center</li>
+                <li>AirCover</li>
+                <li>Anti-discrimination</li>
+                <li>Disability support</li>
+                <li>Cancellation options</li>
+                <li>Report neighborhood concern</li>
+            </ul>
+            <ul>
+                <b> Hosting</b>
+                <li>Airbnb your home</li>
+                <li>AirCover for hosts </li>
+                <li>Hosting resources</li>
+                <li>Community forum</li>
+                <li>Airbnb-friendly apartments</li>
+                <li>Join a free Hosting class</li>
+            </ul>
+            <ul>
+                <b> Airbnb</b>
+                <li>Newsroom</li>
+                <li>Careers </li>
+                <li>Investors</li>
+                <li>Gift cards</li>
+                <li>Airbnb-friendly apartments</li>
+                
+            </ul>
+        </div>
+        <div className={styles.footerLower}>
+            &copy;<span>{ new Date().getFullYear()}</span>{" "} Airbnb, Inc.
+        </div>
+      
+    </footer>
+  )
+}
---
+++ src/components/layout/Navbar/index.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+import styles from "./navbar.module.css"
+import React from 'react'
+import { FaHamburger, FaUserTag } from "react-icons/fa"
+import { FaCircleUser } from "react-icons/fa6"
+import { GiHamburgerMenu } from "react-icons/gi"
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+        <div className={styles["navbar--upper"]}>
+        <Image width={100} height={100} src={'/logo.png'} alt="logo" className={styles.logo} />
+        <div> 
+          <ul className={styles.options}>
+            <li>Stays</li>
+            <li>Experiences</li>
+          </ul>
+        </div>
+        <div className={styles.profile}>
+          <GiHamburgerMenu size={18} />
+          <FaCircleUser size={28}/>
+        </div>
+        </div>
+        <ul className={styles["navbar--lower"]}>
+          <li><div>
+            <b>Where</b><span>Search Destination</span>
+            </div></li>
+          <li style={{
+            fontSize:'2rem'
+          }}>|</li>
+          <li><div>
+            <b> Date</b><span>Add Dates </span>
+            </div></li>
+            <li style={{
+            fontSize:'2rem'
+          }}>|</li>
+          <li><div>
+            <b>Who</b><span>Add guests</span>
+          </div>
+          </li>
+        </ul>
+    </nav>
+  )
+}
